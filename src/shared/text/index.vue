efd0dccd5ba32de1109199a5020b313c9a51934e
Polina <alllrrw@gmail.com>
2025-06-26 12:38:07 +0700

    feat: add text component

---
+++ src/shared/text/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+interface Props {
+    tagName: 'p' | 'h1' | 'h2';
+}
+
+const props = defineProps<Props>();
+
+</script>
+
+<template>
+    <component :is="props.tagName" :class="[props.variant && `text-${props.variant}`]"><slot></slot></component>
+</template>
