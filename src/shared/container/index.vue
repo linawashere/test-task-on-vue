b80bdf41891f71c3a7be19407964efdbc8fa6e40
Polina <alllrrw@gmail.com>
2025-06-26 12:36:52 +0700

    feat: add container

---
+++ src/shared/container/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container"><slot></slot></div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/breakpoints.scss';
+
+.container {
+    max-width: 1720px;
+    margin: 0 auto;
+
+    @include desktop-only {
+        max-width: 1280px;
+    }
+
+    @include tablet-only {
+        max-width: 688px;
+    }
+
+    @include mobile-only {
+        max-width: 290px;
+    }
+}
+</style>
