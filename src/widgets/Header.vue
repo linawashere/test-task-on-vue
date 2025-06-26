ff21692ff15db0a77348d72215730a53c3507edc
Polina <alllrrw@gmail.com>
2025-06-26 16:35:45 +0700

    feat: add part of header

---
+++ src/widgets/Header.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import Container from '@/shared/container/index.vue'
+import Logo from '@/shared/logo/index.vue'
+import Typography from '@/shared/text/index.vue'
+</script>
+<template>
+    <header class="header">
+        <Container class="header__cotainer">
+            <div class="header__logo-description">
+                <div class="header__company-logo">
+                    <Typography tagName="p" class="text-company-logo">
+                        Scamacca
+                    </Typography>
+                    <Logo />
+                </div>
+
+                <Typography tagName="p" class="text-logo-description">
+                    Официальный дилер Scamacca в Москве
+                </Typography>
+            </div>
+
+        </Container>
+    </header>
+</template>
+
+<style scoped>
+@import '@/assets/styles/header.scss';
+</style>
