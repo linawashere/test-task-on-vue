a7b8d69dd9425543023154e9acc750af4657d825
Polina <alllrrw@gmail.com>
2025-06-26 16:34:18 +0700

    feat: add button component

---
+++ src/shared/button/index.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+interface Props {
+    type:
+    | 'primary-black'
+    | 'primary-light'
+    | 'secondary-black'
+    | 'secondary-light',
+    size?: 'sm' | 'lg',
+    disabled?: boolean,
+    rightIcon?: HTNlElement
+}
+
+const props = defineProps<Props>();
+const {
+    type = 'primary-black',
+    size = 'lg',
+    disabled = false
+} = props;
+
+const buttonClasses = {
+    [type]: true,
+    [`size-${size}`]: true,
+    'is-disabled': disabled
+};
+
+</script>
+<template>
+    <button :class="buttonClasses" class="button" :disabled="disabled">
+        <slot></slot>
+        <slot name="rightIcon"></slot>
+    </button>
+</template>
+
+<style scoped>
+@import '@/assets/styles/button.scss';
+</style>
