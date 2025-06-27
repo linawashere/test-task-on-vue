c421f3ac401a3072fe0de5d9e813353d6a9aad58
Polina <alllrrw@gmail.com>
2025-06-27 16:04:37 +0700

    feat: add stats component

---
+++ src/widgets/Stats.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import Icon from '@/shared/btn-icon/index.vue'
+
+interface Props {
+    data: {
+        icon: string;
+        count: number;
+    }[]
+}
+
+const { data } = defineProps<Props>();
+</script>
+
+<template>
+    <div class="header__stats-item" v-for="item in data" :key="item.icon">
+        <Icon :type="item.icon" />
+        <span>{{ item.count }}</span>
+    </div>
+
+</template>
+
+<style scoped>
+@import '@/assets/styles/header.scss';
+</style>
