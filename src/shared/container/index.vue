16a24f949d70d72c52e991e1e2edb546209da548
Polina <alllrrw@gmail.com>
2025-06-27 11:49:37 +0700

    refactor: move container to different folders

--- src/shared/container/index.vue
+++ src/shared/container/index.vue
@@ -1,24 +1,9 @@
 <template>
-    <div class="container"><slot></slot></div>
+    <div class="container">
+        <slot></slot>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-@import '@/assets/styles/breakpoints.scss';
-
-.container {
-    max-width: 1720px;
-    margin: 0 auto;
-
-    @include desktop-only {
-        max-width: 1280px;
-    }
-
-    @include tablet-only {
-        max-width: 688px;
-    }
-
-    @include mobile-only {
-        max-width: 290px;
-    }
-}
+@import '@/assets/styles/container.scss';
 </style>
