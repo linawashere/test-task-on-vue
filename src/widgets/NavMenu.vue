43994018b7ccade8e6602fd5b6d9514eb3311ade
Polina <alllrrw@gmail.com>
2025-06-27 16:05:19 +0700

    fix: fix typography in navigation

--- src/widgets/NavMenu.vue
+++ src/widgets/NavMenu.vue
@@ -16,7 +16,7 @@ const { data } = defineProps<Props>();
     <nav class="header__nav">
         <ul class="header__nav-list">
             <li class="header__nav-item" v-for="item in data" :key="item.text">
-                <Typography tagName="p">{{ item.text }}</Typography>
+                <Typography tagName="p" class="text-body-regular-m">{{ item.text }}</Typography>
                 <Icon :type="item.icon" />
             </li>
         </ul>
