6b49a23ded052b64950e6651c8483ef912929d82
Polina <alllrrw@gmail.com>
2025-06-27 11:52:35 +0700

    feat: add navigation to header

--- src/widgets/Header.vue
+++ src/widgets/Header.vue
@@ -2,10 +2,20 @@
 import Container from '@/shared/container/index.vue'
 import Logo from '@/shared/logo/index.vue'
 import Typography from '@/shared/text/index.vue'
+import NavMenu from '@/widgets/NavMenu.vue'
+import { reactive } from 'vue'
+
+const navItems = reactive([
+    { text: 'Модельный ряд', icon: 'fi-rr-angle-small-down' },
+    { text: 'Покупка', icon: 'fi-rr-angle-small-down' },
+    { text: 'Сервис', icon: 'fi-rr-angle-small-down' },
+    { text: 'О нас', icon: 'fi-rr-angle-small-down' },
+    { text: 'Еще', icon: 'fi-rr-angle-small-down' },
+])
 </script>
 <template>
     <header class="header">
-        <Container class="header__cotainer">
+        <Container class="header__container">
             <div class="header__logo-description">
                 <div class="header__company-logo">
                     <Typography tagName="p" class="text-company-logo">
@@ -18,6 +28,8 @@ import Typography from '@/shared/text/index.vue'
                     Официальный дилер Scamacca в Москве
                 </Typography>
             </div>
+
+                <NavMenu :data="navItems" />
 
         </Container>
     </header>
---
+++ src/widgets/NavMenu.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import Typography from '@/shared/text/index.vue'
+import Icon from '@/shared/btn-icon/index.vue'
+
+interface Props {
+    data: {
+        text: string;
+        icon: string;
+    }[]
+}
+
+const { data } = defineProps<Props>();
+</script>
+
+<template>
+    <nav class="header__nav">
+        <ul class="header__nav-list">
+            <li class="header__nav-item" v-for="item in data" :key="item.text">
+                <Typography tagName="p">{{ item.text }}</Typography>
+                <Icon :type="item.icon" />
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+@import '@/assets/styles/nav-menu.scss';
+</style>
