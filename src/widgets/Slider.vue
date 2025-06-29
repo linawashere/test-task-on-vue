0c67cbbab7e032ef603bc537d981c765b7e8b9c5
Polina <alllrrw@gmail.com>
2025-06-29 12:52:20 +0700

    feat: add slider in main banner

---
+++ src/widgets/Slider.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { reactive } from 'vue'
+import 'swiper/scss'
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { ref } from 'vue'
+
+interface Props {
+    slides: { image: string; alt: string }[];
+}
+
+const props = defineProps<Props>();
+const modules = [Navigation, Pagination, Autoplay];
+</script>
+
+<template>
+    <div class="slider">
+    <Swiper
+      :modules="modules"
+      :navigation="true"
+      :pagination="{ clickable: true }"
+      :loop="true"
+      :autoplay="{
+        delay: 4000,
+        disableOnInteraction: false,
+      }"
+    >
+      <SwiperSlide v-for="(slide, index) in slides" :key="index">
+        <img :src="slide.image" :alt="slide.alt" class="slider__image" />
+      </SwiperSlide>
+    </Swiper>
+  </div>
+</template>
+
+<style scoped>
+@import 'swiper/swiper-bundle.css';
+@import '@/assets/styles/slider.scss';
+</style>
