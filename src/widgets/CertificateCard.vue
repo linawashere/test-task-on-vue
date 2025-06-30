4922cb5dba70d0183720d19a71242eacbf4d11d1
Polina <alllrrw@gmail.com>
2025-06-30 17:43:48 +0700

    feat: add certificate components

---
+++ src/widgets/CertificateCard.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+import Typography from '@/shared/text/index.vue'
+import Button from '@/shared/button/index.vue'
+import Icon from '@/shared/btn-icon/index.vue'
+import CertificateIcon from '@/shared/certificate-icon/index.vue'
+
+interface Certificate {
+    title: string;
+}
+
+const props = defineProps<{
+    certificate: Certificate;
+}>();
+</script>
+
+<template>
+    <div v-if="certificate" class="certificate-card">
+        <div class="certificate-card__content">
+            <div class="certificate-card__text">
+                <Typography tagName="p" class="text-title-m certificate-card__content-title">{{ certificate.title }}
+                </Typography>
+                <Button @click="openCertificate" type="secondary-black">
+                    <template v-slot:rightIcon>
+                        <Icon type="rightIcon" />
+                    </template>
+                    Открыть сертификат
+                </Button>
+            </div>
+            <div class="certificate-card__icon">
+                <CertificateIcon></CertificateIcon>
+            </div>
+        </div>
+        <img class="certificate-card__img" src="/certificate.jpg" alt="Сертификат" />
+    </div>
+</template>
---
+++ src/widgets/CertificatesSection.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import { Navigation, Pagination, EffectFade } from 'swiper/modules'
+import CertificateCard from './CertificateCard.vue'
+import Container from '@/shared/container/index.vue'
+import Typography from '@/shared/text/index.vue'
+import NavigationButtons from '@/shared/nav-btn/index.vue'
+import Button from '@/shared/button/index.vue'
+import Icon from '@/shared/btn-icon/index.vue'
+
+const certificates = [
+    {
+        title: 'Каталог оборудования ECORESULT и ECOWAY',
+    },
+    {
+        title: 'Каталог оборудования ECORESULT и ECOWAY',
+    },
+    {
+        title: 'Каталог оборудования ECORESULT и ECOWAY',
+    },
+    {
+        title: 'Каталог оборудования ECORESULT и ECOWAY',
+    },
+    {
+        title: 'Каталог оборудования ECORESULT и ECOWAY',
+    },
+];
+</script>
+<template>
+    <section class="certificates-section">
+        <Container>
+            <div class="certificates-section__title">
+                <Typography tagName="h2">Сертификаты</Typography>
+                <div class="certificates-section__title-btns">
+                    <NavigationButtons></NavigationButtons>
+                    <Button type="secondary-black">
+                        <template v-slot:rightIcon>
+                            <Icon type="rightIcon" />
+                        </template>
+                        Все услуги
+                    </Button>
+                </div>
+            </div>
+
+            <Swiper :modules="[EffectFade, Pagination, Navigation]" :slides-per-view="3" :space-between="20"
+                :breakpoints="{
+                    1440: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                    1440: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    }
+                }">
+                <SwiperSlide v-for="(certificate, index) in certificates" :key="index">
+                    <CertificateCard :certificate="certificate" />
+                </SwiperSlide>
+            </Swiper>
+        </Container>
+    </section>
+</template>
+
+<style>
+@import '@/assets/styles/certificates.scss';
+</style>
