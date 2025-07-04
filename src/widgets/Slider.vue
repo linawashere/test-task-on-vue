553e1dae0bb2d1f9f25e4bc93915ccc3f4293f63
Polina <alllrrw@gmail.com>
2025-07-04 15:29:16 +0700

    feat: add custom components to slider

--- src/widgets/Slider.vue
+++ src/widgets/Slider.vue
@@ -6,7 +6,7 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { ref } from 'vue'
 
 interface Props {
-    slides: { image: string; alt: string }[];
+  slides: { image: string; alt: string }[];
 }
 
 const props = defineProps<Props>();
@@ -14,25 +14,35 @@ const modules = [Navigation, Pagination, Autoplay];
 </script>
 
 <template>
-    <div class="slider">
-    <Swiper
-      :modules="modules"
-      :navigation="true"
-      :pagination="{ clickable: true }"
-      :loop="true"
-      :autoplay="{
-        delay: 4000,
-        disableOnInteraction: false,
-      }"
-    >
+  <div class="slider">
+    <Swiper :modules="modules" :navigation="{
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }" :pagination="{
+      clickable: true,
+      el: '.swiper-pagination',
+      type: 'bullets'
+    }" :loop="true" :autoplay="{
+      delay: 4000,
+      disableOnInteraction: false,
+    }">
       <SwiperSlide v-for="(slide, index) in slides" :key="index">
         <img :src="slide.image" :alt="slide.alt" class="slider__image" />
       </SwiperSlide>
+
+      <div class="controls-container">
+        <div class="swiper-pagination"></div>
+
+        <div class="navigation-buttons">
+          <button class="custom-prev">❮</button>
+          <button class="custom-next">❯</button>
+        </div>
+      </div>
     </Swiper>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss">
 @import 'swiper/swiper-bundle.css';
 @import '@/assets/styles/slider.scss';
 </style>
