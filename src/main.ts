2b09aa43f945fb2fd8398e81278a89bf27237a2b
Polina <alllrrw@gmail.com>
2025-07-06 16:27:52 +0700

    feat: add filter to slider

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { plugin, defaultConfig } from '@formkit/vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(plugin, defaultConfig);
+
+app.mount('#app');
