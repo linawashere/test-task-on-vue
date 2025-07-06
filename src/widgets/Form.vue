29bacdfe68b0443be648aa8db627347b1009a77d
Polina <alllrrw@gmail.com>
2025-07-06 11:59:23 +0700

    feat: add form submission

--- src/widgets/Form.vue
+++ src/widgets/Form.vue
@@ -4,6 +4,22 @@ import '@formkit/themes/genesis'
 import Button from '@/shared/button/index.vue'
 import Icon from '@/shared/btn-icon/index.vue'
 import Typography from '@/shared/text/index.vue'
+import { vMaska } from "maska/vue"
+import { ref } from 'vue'
+
+const isSubmitted = ref(false)
+const formRef = ref()
+
+const handleSubmit = async (formData: FormData) => {
+
+    try {
+        console.log('Form submitted:', formData);
+        isSubmitted.value = true;
+        formRef.value.reset();
+    } catch (error) {
+        console.error('Submission error:', error);
+    }
+};
 
 interface FormData {
     name: string;
@@ -11,30 +27,40 @@ interface FormData {
     acceptTerms: boolean;
 }
 
-const handleSubmit = (formData: FormData) => {
-    if (!formData.acceptTerms) {
-        return;
-    }
-};
 </script>
 
 <template>
-    <FormKit type="form" :actions="false" @submit="handleSubmit">
+    <FormKit ref="formRef" type="form" :actions="false" @submit="handleSubmit"
+        :form-class="isSubmit ? 'form-submitted' : ''">
         <div class="form__top">
-            <FormKit type="text" name="name" label="Имя" validation="required" placeholder="Введите ваше имя" />
+            <FormKit type="text" name="name" label="Имя" validation="required" placeholder="Введите ваше имя"
+                :validation-messages="{
+                    required: 'Поле обязательно для заполнения'
+                }" />
 
-            <FormKit type="tel" name="phone" label="Телефон" validation="required|length:10"
-                placeholder="+7 (___) ___-__-__" />
+            <FormKit type="tel" name="phone" label="Телефон" v-maska="'+7 (###) ###-##-##'"
+                validation="required|matches:/^\+?7\s?\(\d{3}\)\s?\d{3}-\d{2}-\d{2}$/" placeholder="+7 (___) ___-__-__"
+                :validation-messages="{
+                    required: 'Укажите номер телефона',
+                    matches: 'Введите телефон в формате +7 (XXX) XXX-XX-XX'
+                }" />
         </div>
         <div class="form__bottom">
-        <FormKit type="checkbox" name="acceptTerms" label="Я принимаю условия передачи информации" validation="required" outer-class="terms-checkbox"/>
+            <FormKit type="checkbox" name="acceptTerms" label="Я принимаю условия передачи информации"
+                :validation-messages="{
+                    required: 'Поле обязательно для заполнения'
+                }" validation="required" outer-class="terms-checkbox" />
 
-        <Button type="primary-black">
-            <template v-slot:rightIcon>
-                <Icon type="rightIcon" />
-            </template>
-            Оставить заявку
-        </Button>
-    </div>
+            <Button type="primary-black">
+                <template v-slot:rightIcon>
+                    <Icon type="rightIcon" />
+                </template>
+                Оставить заявку
+            </Button>
+        </div>
+
+        <div v-if="isSubmitted" class="form-success-message">
+            Ваша заявка успешно отправлена!
+        </div>
     </FormKit>
 </template>
